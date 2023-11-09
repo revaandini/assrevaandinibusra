1c7a25d6b8370465707e33d0c1acf286f9deb499
revaandini <revaandini931@gmail.com>
2023-11-09 16:19:51 +0700

    comment

---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kursus Bahasa</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body class="bg-gray-100">
+
+    <header class="bg-blue-500 p-4 text-white">
+        <div class="container mx-auto flex justify-between items-center">
+            <h1 class="text-2xl font-bold">Selamat Datang di Kursus Bahasa</h1>
+            <ul class="space-x-6">
+                <li><a href="#" class="hover:underline">Beranda</a></li>
+                <li><a href="#" class="hover:underline">Kursus</a></li>
+                <li><a href="#" class="hover:underline">Tentang Kami</a></li>
+                <li><a href="#" class="hover:underline">Kontak</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <section class="bg-gray-100 py-12">
+        <div class="container mx-auto text-center">
+            <h2 class="text-3xl font-bold text-gray-800">Kursus Bahasa Unggulan Kami</h2>
+            <p class="text-lg text-gray-600 mt-4">Kami menawarkan berbagai kursus bahasa berkualitas dengan instruktur yang berpengalaman.</p>
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-12">
+                <div class="p-6 bg-white border rounded-lg shadow-md">
+                    <h3 class="text-xl font-semibold text-gray-800">Kursus Bahasa Inggris</h3>
+                    <p class="text-gray-600 mt-2">Pelajari bahasa Inggris dari instruktur yang berpengalaman.</p>
+                </div>
+                <div class="p-6 bg-white border rounded-lg shadow-md">
+                    <h3 class="text-xl font-semibold text-gray-800">Kursus Bahasa Spanyol</h3>
+                    <p class="text-gray-600 mt-2">Jelajahi keindahan bahasa Spanyol bersama kami.</p>
+                </div>
+                <div class="p-6 bg-white border rounded-lg shadow-md">
+                    <h3 class="text-xl font-semibold text-gray-800">Kursus Bahasa Prancis</h3>
+                    <p class="text-gray-600 mt-2">Mendalami bahasa Prancis untuk tujuan pribadi atau profesional.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer class="bg-blue-500 py-4 text-white text-center">
+        <div class="container mx-auto">
+            <p>&copy; 2023 Kursus Bahasa - Hak Cipta Dilindungi</p>
+        </div>
+    </footer>
+
+</body>
+</html>
